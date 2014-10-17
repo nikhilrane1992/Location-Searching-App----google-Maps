b519a9c3e33adcbaf2d685329071842b5babf01f
Nikhil Rane <nikhilrane1992@gmail.com>
2014-10-17 12:07:29 +0530

    Checking the device for the Google Play services APK

--- Map/src/com/bluerocket/map/MainActivity.java
+++ Map/src/com/bluerocket/map/MainActivity.java
@@ -1,16 +1,27 @@
 package com.bluerocket.map;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	GoogleMap mMap;
+	private static final int GPS_ERRORDIALOG_REQUEST = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (serviceOK()){
+			Toast.makeText(this, "Ready to Use MAP", Toast.LENGTH_SHORT).show();
+
+		}
         setContentView(R.layout.activity_main);
     }
 
@@ -33,4 +44,19 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    public boolean serviceOK()
+	{
+		int isAvailables = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (isAvailables == ConnectionResult.SUCCESS){
+			return true;
+		}else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailables)){
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailables, this, GPS_ERRORDIALOG_REQUEST);
+			dialog.show();
+		}
+		else{
+			Toast.makeText(this, "Google service not available", Toast.LENGTH_SHORT).show();
+		}
+		return false;
+	}
 }
